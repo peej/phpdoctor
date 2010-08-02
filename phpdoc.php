6c68683224c00339c9d01013b9c1faa845e696dd
DerManoMann <mano@zenmagick.org>
2010-08-03 05:42:26 +0800

    disable error reporting to avoid deprecated warnings using PHP5.3

--- phpdoc.php
+++ phpdoc.php
@@ -19,6 +19,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+error_reporting(0);
+
 // check we are running from the command line
 if (!isset($argv[0])) {
     die('This program must be run from the command line using the CLI version of PHP');
